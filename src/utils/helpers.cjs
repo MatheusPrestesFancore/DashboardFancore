ec7c53dbf9dc5f51fe13a48ea4fd9fb34e7fc5eb
MatheusPrestesFancore <matheus.prestes@fancoregroup.com.br>
2025-08-26 15:18:19 -0300

    Atualizando definição de leads qualificados

--- src/utils/helpers.cjs
+++ src/utils/helpers.cjs
@@ -1,6 +1,6 @@
 export const QUALIFIED_STAGES = [
-  'Data_Contato Feito', 'Data_Reunião agendada', 'Data_Reunião realizada', 'Data_COF enviada',
-  'Data_COF assinada', 'Data_Venda', 'Data_Noshow'
+  'Data_Contato feito', 'Data_Reunião agendada', 'Data_Reagendar', 'Data_Noshow',
+  'Data_Reunião feita', 'Data_COF enviada','Data_COF Assinada', 'Data_Negociação', 'Data_Pré- contrato', 'Data_Stand By (qualif.)', 'Data_Venda'
 ];
 
 // Define as palavras-chave que indicam interesse.
