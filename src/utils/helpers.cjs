b06ffee75f4fe4cdb8147d678d9f6d9713d92603
MatheusPrestesFancore <matheus.prestes@fancoregroup.com.br>
2025-08-27 16:20:49 -0300

    correção basica.

--- src/utils/helpers.cjs
+++ src/utils/helpers.cjs
@@ -1,6 +1,6 @@
 export const QUALIFIED_STAGES = [
   'Data_Contato feito', 'Data_Reunião agendada', 'Data_Reagendar', 'Data_Noshow',
-  'Data_Reunião feita', 'Data_COF enviada','Data_COF Assinada', 'Data_Negociação', 'Data_Pré- contrato', 'Data_Stand By (qualif.)', 'Data_Venda'
+  'Data_Reunião feita', 'Data_COF Enviada', 'Data_COF Assinada', 'Data_Negociação', 'Data_Pré- contrato', 'Data_Stand By (qualif.)', 'Data_Venda'
 ];
 
 // Define as palavras-chave que indicam interesse.
