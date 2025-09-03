b34e0c95ada1bba83cc26d5f648e3b3eb44830ad
MatheusPrestesFancore <matheus.prestes@fancoregroup.com.br>
2025-09-03 17:29:46 -0300

    alterando cor do mapa.

--- src/pages/MapaVendasDashboard.jsx
+++ src/pages/MapaVendasDashboard.jsx
@@ -103,8 +103,9 @@ const MapaVendasDashboard = ({ data }) => {
       <div className="lg:col-span-2 rounded-lg overflow-hidden shadow-lg">
         <MapContainer center={initialPosition} zoom={4} style={{ height: '100%', width: '100%' }} scrollWheelZoom={true}>
           <ChangeView center={selectedCity ? [selectedCity.lat, selectedCity.lng] : initialPosition} zoom={selectedCity ? 13 : 4} />
+          {/* ALTERADO: A URL do TileLayer foi trocada para um tema mais claro e moderno */}
           <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
           />
           {data.map(city => (
